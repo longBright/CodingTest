4ceda6a5f1cceccf41d46e7b389eb04a2e51f4d0
dudghks97 <yh9638@skuniv.ac.kr>
2022-05-11 17:57:19 +0900

    22.05.11

--- BaekJoon/01_greedy/2217.py
+++ BaekJoon/01_greedy/2217.py
@@ -1,3 +1,22 @@
 # 2217번
 # 로프
 # https://www.acmicpc.net/problem/2217
+
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(int(input()))
+
+for w in range(10000):
+    if w // n >= min(data):
+        break
+print(w)
+
+
+data.sort(reverse=True)
+#result = []
+result = 0
+for i in range(n):
+    #result.append(data[i]*(i+1))
+    result = max(result, data[i]*(i+1))
+#print(max(result))
